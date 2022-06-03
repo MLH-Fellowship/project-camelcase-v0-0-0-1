7ddc1fd5627986cf2784710d5f1bb418e375df7c
Dario <darioarias12@gmail.com>
2022-06-02 21:59:04 -0400

    update to templates, everything is broken down into pieces, base template supports bootstrap and bootstrap-icons

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,11 @@ import os
 @main.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@main.route('/gabby')
+def about_gabby():
+    return render_template('about_gabby.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@main.route('/dario')
+def about_dario():
+    return render_template('about_dario.html', title="MLH Fellow", url=os.getenv("URL"))
