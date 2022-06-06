d3e507c7dacad11ef37e9fffbd4a73805b9cd47a
gabyjazzmin <gabrielaj.4.f@gmail.com>
2022-06-06 15:23:31 -0500

    Add information

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,4 @@ import os
 
 @main.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Meet us", url=os.getenv("URL"))
