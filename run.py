7c69cea030dacc56f2f01175d157b6501615a770
Dario <darioarias12@gmail.com>
2022-06-27 10:22:00 -0400

    added cli support for deploying application-db

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 from app import create_app
 import os
+from app import db
+from app.models import TimelinePost
 
 app = create_app(config_profile=os.getenv('FLASK_CONFIG') or 'development')
+
+@app.shell_context_processor
+def make_shell_context():
+  print('making context')
+  return dict(db=db, app=app, TimelinePost=TimelinePost)
+
+@app.cli.command()
+def deploy():
+  print('Deploying db')
+  db.connect();
+  db.create_tables([TimelinePost])
