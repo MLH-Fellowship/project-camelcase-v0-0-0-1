0805f12336ff04239dabd2ffe99851680b96e25b
gabyjazzmin <gabrielaj.4.f@gmail.com>
2022-07-20 21:20:43 -0500

    Timeline Ask 3 data

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,17 @@ bootstrap.init_app(app)
 
 #print("DATOS ++++++++",os.getenv("URL"))
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306
-)
+if os.getenv("TESTING")=="true":
+    print("Running in test mode")
+    mydb= SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
+    
 print(mydb)
 
 @app.route('/')
