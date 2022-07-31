bab98107228407259891446384cb5fdbb92eaa22
gabyjazzmin <gabrielaj.4.f@gmail.com>
2022-07-31 13:36:47 -0500

    fix redeplot

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd project-camelcase-v0-0-0-1
+git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
-flask run --host=0.0.0.0
+
