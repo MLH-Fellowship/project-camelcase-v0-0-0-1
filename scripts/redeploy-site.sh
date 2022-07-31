09f3d5b25ad8288578c79f12fd2a5087f5ce0780
gabyjazzmin <gabrielaj.4.f@gmail.com>
2022-07-31 13:15:40 -0500

    update redeploy for docker

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd project-camelcase-v0-0-0-1
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
